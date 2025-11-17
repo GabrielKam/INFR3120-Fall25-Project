553d423c28eeb23cff0670085eedce694c0a1ad8
Alexander Waiganjo <alexander.waiganjo@ontariotechu.net>
2025-11-16 23:56:27 -0500

    Read File

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 const route = express.Router();
 import { homeController, createController } from '../controllers/homeController.js';
+import StudentModel from '../models/schema.js';
 
 route.get('/', homeController);
 route.post('/', createController);
@@ -11,6 +12,16 @@ route.get('/delete', function(req, res, next) {
 
 route.get('/update', function(req, res, next) {
   res.render('update', { title: 'Update',displayName: req.user?req.user.displayName:"" });
+});
+
+route.get('/read', async(req, res) =>{
+  try{
+    const studentData = await StudentModel.find();
+    res.render('read', {title: "Read", studentData});
+  } catch (err) {
+    res.send("Somethings Wrong");
+  }
+  
 });
 
 export default route;
