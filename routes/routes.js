71df98ea5d3e115b38ffa8a7dc4083afdf67a1fc
Alexander Waiganjo <alexander.waiganjo@ontariotechu.net>
2025-11-16 21:49:19 -0500

    Added Update
    fixed some syntax errors in the delete page aswell

--- routes/routes.js
+++ routes/routes.js
@@ -5,7 +5,11 @@ import { homeController } from '../controllers/homeController.js';
 route.get('/', homeController);
 
 route.get('/delete', function(req, res, next) {
-  res.render('delete', { title: 'About us',displayName: req.user?req.user.displayName:"" });
+  res.render('delete', { title: 'Delete',displayName: req.user?req.user.displayName:"" });
+});
+
+route.get('/update', function(req, res, next) {
+  res.render('update', { title: 'Update',displayName: req.user?req.user.displayName:"" });
 });
 
 export default route;
